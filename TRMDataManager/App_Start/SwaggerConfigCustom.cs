4e653f1999b1cfd6604ac19648bfed62f681a611
Samrat Ghosh <samratdot@outlook.com>
2020-10-02 00:10:33 +0530

    Add Sql server project

---
+++ TRMDataManager/App_Start/SwaggerConfigCustom.cs
@@ -0,0 +1,47 @@
+﻿using Swashbuckle.Swagger;
+using System.Collections.Generic;
+using System.Web.Http.Description;
+
+namespace TRMDataManager.App_Start
+{
+    public class SwaggerAuthTokenOperation : IDocumentFilter
+    {
+        public void Apply(SwaggerDocument swaggerDoc, SchemaRegistry schemaRegistry, IApiExplorer apiExplorer)
+        {
+            swaggerDoc.paths.Add("/token", new PathItem()
+            {
+                post = new Operation
+                {
+                    tags = new List<string> { "Auth" },
+                    consumes = new List<string> { "application/x-www-form-urlencoded" },
+                    parameters = new List<Parameter>
+                    {
+                        new Parameter
+                        {
+                            type = "string",
+                            name = "grant_type",
+                            required = true,
+                            @in = "formdata",
+                            @default = "password"
+                        },
+                        new Parameter
+                        {
+                            type = "string",
+                            name = "username",
+                            required = false,
+                            @in = "formdata"
+                        },
+                        new Parameter
+                        {
+                            type = "string",
+                            name = "password",
+                            required = false,
+                            @in = "formdata"
+                        },
+
+                    }
+                }
+            });
+        }
+    }
+}
